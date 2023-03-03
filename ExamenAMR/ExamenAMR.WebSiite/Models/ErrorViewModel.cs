782e56ca4a0c70e4f3e574f38a9727ebbb7ad8e2
AmadoMataRosales <amadomata@hotmail.com>
2023-03-02 21:54:20 -0600

    Carga Inicial

---
+++ ExamenAMR/ExamenAMR.WebSiite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenAMR.WebSiite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
